fc2d5b71e4119936f76c294e7f11d83de14b36bb
akshith227 <akshith272727@gmail.com>
2021-06-22 18:26:47 +0530

    using datatypes, will build a basic calculator using loops for verification of strings and perhaps a simple gui next

---
+++ using_datatypes/datatypes.rb
@@ -0,0 +1,5 @@
+str = "hi"
+num = 78
+float = 9.1
+booleans = [true, false, nil]
+puts (num*float)
---
+++ using_datatypes/strings.rb
@@ -0,0 +1,8 @@
+str = "  ruby on rails"
+puts str.upcase()
+puts str.strip()
+puts str.length()
+puts str.include? "on"
+puts str.include? "e"
+puts str.index("o")
+#puts str.index("z")
